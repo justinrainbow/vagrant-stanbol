4dfa4cbb0220d9ca32ecfc1b03d6cb00182e0983
Justin Rainbow <justin.rainbow@gmail.com>
2012-01-08 20:08:23 -0700

    Adding README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Stanbol in Vagrant
+
+
+## Installation
+
+``` sh
+git clone git://github.com/justinrainbow/vagrant-stanbol.git
+cd vagrant-stanbol
+vagrant up
+```
+
+## Prerequists
+
+``` sh
+sudo gem install vagrant
+```
+
+It's possible that you will need to correct the private key permissions on
+the Vagrant private key.  For example, on OS X you would run:
+
+``` sh
+sudo chmod 644 /Library/Ruby/Gems/1.8/gems/vagrant-0.8.*/keys/vagrant
+```
--- manifests/base.pp
+++ manifests/base.pp
@@ -5,7 +5,34 @@ Exec {
     path => "/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
 }
 
+class init {
+  group { "puppet":
+      ensure => "present",
+  }
+
+  case $operatingsystem {
+      ubuntu: {
+          exec { "update_apt":
+              command => "sudo apt-get update",
+          }
+
+          # Provides "add-apt-repository" command, useful if you need
+          # to install software from other apt repositories.
+          package { "python-software-properties":
+              ensure => present,
+              require => [
+                  Exec['update_apt'],
+              ];
+          }
+      }
+  }
+}
+
 class dev {
+    class {
+      init: before => Class['stanbol']
+    }
+
     include stanbol
 }
 
